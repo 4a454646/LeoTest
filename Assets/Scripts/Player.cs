e6df32e308a7459f234019e2d434ab9e9c4f43ef
Jeff <4a.45.46.46@gmail.com>
2023-07-07 16:57:33 -0700

    basic player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
+    
+    [Header("Player Movement")]
+    [SerializeField] private float maxVel = 0.3f;
+    [SerializeField] private float acceleration = 0.05f;
+    [SerializeField] private float deceleration = 0.5f;
+    [SerializeField] private float currentSpeed = 0.0f;
+    
+    private void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update() {
+    
+    private void Update() {
         
     }
+
+    private void FixedUpdate() { 
+        float horizontal = Input.GetAxis("Horizontal");
+        if (horizontal == 0) {
+            currentSpeed = currentSpeed * deceleration;
+            if (currentSpeed < 0.01f && currentSpeed > -0.01f) {
+                currentSpeed = 0;
+            }
+        }
+        else {
+            currentSpeed = currentSpeed + horizontal * acceleration;
+            currentSpeed = Mathf.Clamp(currentSpeed, -maxVel, maxVel);
+        }
+        transform.position = new Vector3(
+            transform.position.x + currentSpeed, 
+            transform.position.y,
+            0
+        );
+    }    
 }

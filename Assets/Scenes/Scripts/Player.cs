00f0ee18c5611340624b076464fe6a57ad3ebf76
dkim7405 <dongkim7405@gmail.com>
2023-07-07 16:28:09 -0700

    TempCommit
    temp

--- Assets/Scenes/Scripts/Player.cs
+++ Assets/Scenes/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //helloworld
     }
 
     // Update is called once per frame

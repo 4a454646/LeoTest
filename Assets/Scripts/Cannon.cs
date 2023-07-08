e45387b77e3ffedd6a95132d8c2440463a586115
Jeff <4a.45.46.46@gmail.com>
2023-07-07 18:26:00 -0700

    new cannon

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -17,8 +17,13 @@ public class Cannon : MonoBehaviour {
     [SerializeField] private float attackDelay = 0.5f;
     [SerializeField] private float fastAttackDelay = 0.5f;
     [SerializeField] private float defaultAttackDelay = 0.5f;
+    [SerializeField] private Coroutine refreshAttackCoro;
 
+    [SerializeField] private bool isTimeSlowed = false;
     [SerializeField] private float timeSlow;
+    [SerializeField] private int timeSlowAvailable = 100;
+    [SerializeField] private int maxTimeSlow = 100;
+    [SerializeField] private int timeSlowDecrease = 2;
 
     private void Start() {
         
@@ -35,22 +40,41 @@ public class Cannon : MonoBehaviour {
         if (Input.GetMouseButton(0)) {
             if (canAttack) {
                 canAttack = false;
-                StartCoroutine(RefreshAttack());
+                refreshAttackCoro = StartCoroutine(RefreshAttack());
                 StartCoroutine(FireProjectile());
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && timeSlowAvailable > 0) {
+            StopCoroutine(refreshAttackCoro);
             canAttack = true;
             attackDelay = fastAttackDelay;
             animDelay = fastAnimDelay;
             Time.timeScale = timeSlow;
+            isTimeSlowed = true;
         }
         else if (Input.GetKeyUp(KeyCode.Space)) {
-            attackDelay = defaultAttackDelay;
-            animDelay = defaultAnimDelay;
-            Time.timeScale = 1;
+            ResetTimeSpeed();
         }
+    }
+
+    private void FixedUpdate() {
+        if (isTimeSlowed) {
+            timeSlowAvailable -= timeSlowDecrease;
+            if (timeSlowAvailable < 0) {
+                timeSlowAvailable = 0;
+                ResetTimeSpeed();
+            }
+        }
+    }
+
+    private void ResetTimeSpeed() {
+        attackDelay = defaultAttackDelay;
+        animDelay = defaultAnimDelay;
+        Time.timeScale = 1;
+        isTimeSlowed = false;
+        StopCoroutine(refreshAttackCoro);
+        canAttack = true;
     }
 
     private IEnumerator RefreshAttack() {
@@ -61,19 +85,20 @@ public class Cannon : MonoBehaviour {
     private IEnumerator FireProjectile() {
         yield return new WaitForSeconds(animDelay);
         // squash, then unsquash the cannon for added visual effect
-        transform.localScale = new Vector3(0.85f, 1.0f, 1.0f);
+        transform.localScale = new Vector3(0.85f, 1.05f, 1.0f);
         yield return new WaitForSeconds(animDelay);
-        transform.localScale = new Vector3(0.65f, 1.0f, 1.0f);
+        transform.localScale = new Vector3(0.65f, 1.2f, 1.0f);
         yield return new WaitForSeconds(animDelay * 2);
-        transform.localScale = new Vector3(0.85f, 1.0f, 1.0f);
-        yield return new WaitForSeconds(animDelay / 2);
-        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        yield return new WaitForSeconds(animDelay);
+        transform.localScale = new Vector3(0.85f, 1.05f, 1.0f);
         GameObject shot = Instantiate(
             cannonball, 
             transform.position + transform.right * cannonballOffset,
             transform.rotation
         );
+        yield return new WaitForSeconds(animDelay / 2);
+        transform.localScale = new Vector3(1.1f, 0.9f, 1.0f);
         shot.GetComponent<Rigidbody2D>().velocity = shot.transform.right * cannonballSpeed;
+        yield return new WaitForSeconds(animDelay / 2);
+        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
     }
 }
